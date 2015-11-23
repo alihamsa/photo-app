5defd4116976af0c27a57f68975041a5e14469ec
Martin Cross <mncross@gmail.com>
2015-11-23 17:46:36 -0600

    create partial and controller for adding photo

---
+++ javascript/addPhoto.js
@@ -0,0 +1,19 @@
+app.controller('addPhotoController', ['$firebaseArray', function($firebaseArray){
+	console.log('addPhotoController hooked up');
+
+	var firebaseRef = new Firebase('https://photo-apps.firebaseio.com/');
+
+	this.photoList = $firebaseArray(firebaseRef);
+
+	this.newPhoto = {};
+
+	this.addPhoto = function(){
+		this.photoList.$add({
+			title: this.newPhoto.title,
+			album: this.newPhoto.album,
+			photoURL: this.newPhoto.photoURL,
+			description: this.newPhoto.description
+		});
+		this.newPhoto = {};
+	}
+}]);
--- javascript/main.js
+++ javascript/main.js
@@ -1,23 +1,20 @@
 var app = angular.module('photoApp', ['ngRoute', 'firebase']);
 
-	app.config(function ($routeProvider) {
-        $routeProvider.
-            when('/myProfile', {templateUrl: 'partials/myProfile.html', controller: 'photoCtrl'}).
-            otherwise({redirectTo: '/index.html'});
-            
-    });
+app.config(['$routeProvider', function ($routeProvider) {
+  $routeProvider
+  .when('/myProfile', {
+    templateUrl: 'partials/myProfile.html',
+    controller: 'photoCtrl'
+  })
+}]);
 
-
- app.controller("photoCtrl", 
-  ["$scope", 
-   "$firebaseArray", 
-  function($scope, $firebaseArray) {
+ app.controller("photoCtrl", ["$scope", "$firebaseArray", function($scope, $firebaseArray) {
 
   // Just like in the RequireJS version of Music History, make a reference
-    var ref = new Firebase("https://photo-apps.firebaseio.com/");
+  var ref = new Firebase("https://photo-apps.firebaseio.com/");
 
-  // Instead of snapshot.val(), use this syntax to get songs
-    $scope.songs = $firebaseArray(ref);
+  // Instead of snapshot.val(), use this syntax to get photos
+  console.log("$firebaseArray(ref)", $firebaseArray(ref));
+  $scope.photos = $firebaseArray(ref);
 
-  }
-]);
+  }]);
