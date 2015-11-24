b78bcd1007ae065ba8d2e56c188d5403c0989527
jtmp2r <jtmp2r@gmail.com>
2015-11-23 19:46:21 -0600

    firebase authentication done

--- javascript/authentication/registerAuth.js
+++ javascript/authentication/registerAuth.js
@@ -1,38 +1,43 @@
-var app = angular.module("PhotoApp", [firebase]);
+var app = angular.module("PhotoApp", ["firebase"]);
+
+app.controller("AuthCtrl", ["$scope", "Auth", function($scope, Auth) { 
 
 
+  	$scope.createUser = function() {
+  		  $scope.message = null;
+        $scope.error = null;
+        console.log("Hell Yeah!!")
 
-
-app.controller("AuthCtrl", ["$scope", "$firebaseAuth",
-
-  function($scope, $firebaseObject, $firebaseAuth, $location) {
-    var ref = new Firebase("https://photo-apps.firebaseio.com/");
-    var $scope.auth = $firebaseAuth(ref);
-
-
-  	$scope.$createUser = function() {
-			  $scope.auth.$createUser({
+			  Auth.$createUser({
 			  email: $scope.email,
 			  password: $scope.password
 			}).then(function(userData) {
 			   console.log("User " + userData.uid + " created successfully!");
-			   console.log($scope.auth);
+			   console.log();
+				}).catch(function(error) {
 
+				});
+        
 
-			  return $scope.auth.$authWithPassword({
-			    email: $scope.email,
-			    password: $scope.password
-			  });
-			}.then(function(authData) {
-			  console.log("Logged in as:", authData.uid);
-			}).catch(function(error) {
-			  console.error("Error: ", error);
-			});
 		}; //end register
 
+		$scope.logIn = function(){
+    	Auth.$authWithPassword({
+			  email: $scope.email,
+			  password: $scope.password
+			}).then(function(authData) {
+				console.log("User: ", Auth.$authWithPassword)
+			}).catch(function(error) {
+			  console.error("Something's amiss:", error);
+			});
+    }; // login
+
+    $scope.logOut = function(){
+    	// console.log('logged out');
+			$scope.auth.$unauth();
+			$location.path( "/");
+		}; // logout
+
+}]);
 
 
-
-
-  } 
-]); // controller end
