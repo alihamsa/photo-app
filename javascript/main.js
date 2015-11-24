8ebf4a2698cbcaa7c7855fe66170165332a72c4b
Martin Cross <mncross@gmail.com>
2015-11-24 16:22:10 -0600

    display photos for authed user

--- javascript/main.js
+++ javascript/main.js
@@ -1,27 +1,35 @@
 var app = angular.module('photoApp', ['ngRoute', 'firebase']);
 
-	app.config(function ($routeProvider) {
-        $routeProvider.
-            when('/myProfile', {templateUrl: 'partials/myProfile.html', controller: 'photoCtrl'}).
-            otherwise({redirectTo: '/index.html'});
-            
-    });
+app.config(function ($routeProvider) {
+  $routeProvider.
+  when('/myProfile', {templateUrl: 'partials/myProfile.html', controller: 'photoCtrl'}).
+  otherwise({redirectTo: '/index.html'});
+  });
 
+app.factory("Auth", ["$firebaseAuth",
+  function($firebaseAuth) {
+    var ref = new Firebase("https://photo-apps.firebaseio.com/");
+    return $firebaseAuth(ref);
+  }
+]);
 
- app.controller("photoCtrl", 
-  ["$scope", 
-   "$firebaseArray", 
-  function($scope, $firebaseArray) {
+app.controller("photoCtrl",
+  ["$scope",
+  "$firebaseArray", 'Auth',
+  function($scope, $firebaseArray, Auth) {
+
+  this.userAuthData = Auth.$getAuth();
 
   // Just like in the RequireJS version of Music History, make a reference
-    var ref = new Firebase("https://photo-apps.firebaseio.com/");
+  var ref = new Firebase('https://photo-apps.firebaseio.com/users/' + this.userAuthData.uid + '/photos');
 
   // Instead of snapshot.val(), use this syntax to get songs
-    $scope.photo = $firebaseArray(ref);
+  $scope.photo = $firebaseArray(ref);
 
+  $scope.editPhoto = function(photo){
+    console.log('editPhoto run', photo);
+  };
 
-   
-
-    }
+  }
 
 ]);
